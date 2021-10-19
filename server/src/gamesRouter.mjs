c191e01b7edbfad4c3ac105ef6ec1695bec75e42
George Song <george@damacy.net>
2021-10-19 09:01:58 -0700

    feat: load data from games API
    * Create got instance to simplify API access

--- server/src/gamesRouter.mjs
+++ server/src/gamesRouter.mjs
@@ -1,16 +1,22 @@
 import express from "express";
 import got from "got";
 
+import { load_dotenv_if_exists } from "./utils.mjs";
+
+load_dotenv_if_exists();
+
 const router = express.Router();
-const api = {
-  url: "https://api.boardgameatlas.com/api/search?name=",
-  key: process.env.GAME_API_KEY,
-};
-router.get("/", async (request, response) => {
-  const { games } = await got(
-    `https://api.boardgameatlas.com/api/search?name=${request.title}&client_id=${api.key}`,
-  );
+
+router.get("/", async ({ query: { name } }, response) => {
+  const { games } = await bga("search", { searchParams: { name } });
   response.json(games);
+});
+
+const bga = got.extend({
+  prefixUrl: "https://api.boardgameatlas.com/api/",
+  responseType: "json",
+  resolveBodyOnly: true,
+  searchParams: { client_id: process.env.GAME_API_KEY },
 });
 
 export default router;
