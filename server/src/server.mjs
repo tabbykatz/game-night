120816635c5843353ee9c6beffe7b918b0965f1b
Tabitha O'Melay <tomelay@gmail.com>
2021-10-20 11:23:40 -0700

    add jwtCheck to games route

--- server/src/server.mjs
+++ server/src/server.mjs
@@ -10,7 +10,7 @@ const app = express();
 
 app.use("/api/tasks", jwtCheck, taskRouter);
 app.use("/api/users", jwtCheck, userRouter);
-app.use("/api/games", gamesRouter);
+app.use("/api/games", jwtCheck, gamesRouter);
 
 // Do not comment out or delete this end point. The React development server
 // won't start until it pings this end point successfully.
