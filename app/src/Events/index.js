d512b6c3f08516cdef342ffc5427e6fce4952c97
Tabitha O'Melay <tomelay@gmail.com>
2021-10-25 20:02:56 -0700

    add rough outline of Events components

---
+++ app/src/Events/index.js
@@ -0,0 +1,64 @@
+import * as React from "react";
+
+import { toast } from "react-hot-toast";
+
+import useApi from "../auth/useApi";
+import Card from "../components/Card";
+// TODO: make this real
+import { useMyEvents } from "../hooks";
+
+import styles from "./styles.module.scss";
+
+const Events = ({ limit }) => {
+  const { myEvents, loadEvents } = useMyEvents();
+  const { apiClient } = useApi();
+
+  const deleteEvent = (event) => {
+    apiClient
+      // TODO: make this real
+      .deleteEvent(event.id)
+      .then(loadEvents())
+      .then(toast("Event removed!"));
+  };
+
+  const createEvent = (event) => {
+    apiClient
+      // TODO: make this real
+      .createEvent(event)
+      .then(loadEvents())
+      .then(toast("Event created!"));
+  };
+
+  return myEvents ? (
+    <>
+      {/* TODO: Add a button to go to addevent */}
+      <section>
+        <EventsList events={myEvents} {...{ deleteEvent }} {...{ limit }} />
+      </section>
+    </>
+  ) : null;
+};
+
+const EventsList = ({ events, deleteEvent, limit }) => {
+  if (limit) {
+    events = events.slice(0, limit);
+  }
+  return (
+    <>
+      <ul className={styles.grid}>
+        {events.map((event) => (
+          <li className={styles.card} key={event.id}>
+            <Card
+              {...{ event }}
+              handleClick={deleteEvent}
+              isIn={true}
+              action={"remove"}
+            />
+          </li>
+        ))}
+      </ul>
+    </>
+  );
+};
+
+export default Events;
