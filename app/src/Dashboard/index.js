0d89722843b66875ec2c218f6f48c1058ba49338
Tabitha O'Melay <tomelay@gmail.com>
2021-11-09 15:11:01 -0800

    add fix home

--- app/src/Dashboard/index.js
+++ app/src/Dashboard/index.js
@@ -34,17 +34,21 @@ const Dashboard = () => {
     </>
   ) : (
     <>
-      <div className={styles.tagline}>
-        <img src="./logo.png" className={styles.logo} alt="" />
-        <h1>Build your collection.</h1>
-      </div>
-      <div className={styles.tagline}>
-        <h1>Create events and invite your friends.</h1>
-        <img src="./logo.png" className={styles.logo} alt="" />
-      </div>
-      <div className={styles.tagline}>
-        <img src="./logo.png" className={styles.logo} alt="" />
-        <h1>See all the games coming to the event and add your own!</h1>
+      <div className={styles.home}>
+        <ul>
+          <li className={styles.tagline}>
+            <img src="./logo.png" className={styles.logo} alt="" />
+            <h1>Build your collection.</h1>
+          </li>
+          <li className={styles.tagline}>
+            <h1>Create events and invite your friends.</h1>
+            <img src="./logo.png" className={styles.logo} alt="" />
+          </li>
+          <li className={styles.tagline}>
+            <img src="./logo.png" className={styles.logo} alt="" />
+            <h1>See all the games coming to the event and add your own!</h1>
+          </li>
+        </ul>
       </div>
     </>
   );
